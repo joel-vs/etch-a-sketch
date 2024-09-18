7df094fa5bbda880e3d6a0d727d1edd2c506813e
Joel V Saju <63978772+joel-vs@users.noreply.github.com>
2024-09-19 00:38:34 +0530

    feat: implement grid resizing with button

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const container = document.querySelector(".container");
-let gridSize = 16;
+const gridSizeButton = document.querySelector(".gridSize");
 
-container.innerHTML = '';
+let gridSize = 25;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    let div = document.createElement('div');
-    container.appendChild(div);
-}
+function createGrid(gridSize) {
+    container.textContent = ''; 
+    let containerSize = Math.min(window.innerWidth, window.innerHeight);
+    let divSize = containerSize / gridSize;
+    
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let div = document.createElement('div');
+        div.style.width = `${divSize}px`
+        div.style.height = `${divSize}px`
+        container.appendChild(div);
+    };
+};    
 
-container.style.width = `${gridSize * 25}px`;
+createGrid(gridSize);
+
+gridSizeButton.addEventListener('click', () => {
+    let input = prompt("Enter the number of square per side (max 100): ")
+    if (input === null) {
+        return;
+    };
+    gridSize = parseInt(input); 
+    if (isNaN(gridSize) || gridSize > 100 || gridSize <= 0) {
+        alert("Please enter a number between 1 and 100.");
+    }
+    else {
+        createGrid(gridSize);
+    };    
+});
+
+window.addEventListener('resize', () => {
+    createGrid(gridSize);
+});
