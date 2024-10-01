a9398ff89ba6473af78045d252f1fac222e8e0c4
Joel V Saju <63978772+joel-vs@users.noreply.github.com>
2024-10-01 12:12:01 +0530

    Complete implementation of etch-a-sketch website

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
 const gridSizeButton = document.querySelector(".gridSize");
 const resetButton = document.querySelector(".reset");
+const randomColorButton = document.querySelector(".randomColor");
 
 let gridSize = 16;
+
+let isOn = false;
 
 function createGrid(gridSize) {
     container.textContent = ''; 
@@ -18,15 +21,55 @@ function createGrid(gridSize) {
     };
 
     const gridSquares = document.querySelectorAll(".gridSquares");
-
     for(let i=0; i<gridSquares.length; i++) {
-        gridSquares[i].addEventListener("mouseover", function(e) {
-            e.currentTarget.style.backgroundColor = "black";
-        });
-    };
-};    
+        if (isOn === true) {
+            gridSquares[i].addEventListener("mouseover", function(e) {
+                e.currentTarget.style.backgroundColor = toggleRandomColor();
+            });
+        }
+        else if (isOn === false) {
+            gridSquares[i].addEventListener("mouseover", function(e) {
+                e.currentTarget.style.backgroundColor = "black";
+            });
+        };
+    };    
+};   
+
+function toggleRandomColor () {
+    let code = '';
+    hexArray = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+    for (let i=0; i<6; i++) {
+        let randomNumber = Math.floor(Math.random() * 16);
+        code += hexArray[randomNumber];
+    }
+    return `#${code}`;
+};
 
 createGrid(gridSize);
+
+randomColorButton.addEventListener('click', () => {
+    isOn = !isOn; //toggle boolean value of isOn.
+    if (isOn === true) {
+        alert("Random color mode is now ON.");
+    }
+    else {
+        alert("Random color mode is now OFF.");
+    };
+
+    const gridSquares = document.querySelectorAll(".gridSquares");
+    for(let i=0; i<gridSquares.length; i++) {
+        if (isOn === false) {
+            gridSquares[i].addEventListener("mouseover", function(e) {
+                e.currentTarget.style.backgroundColor = "black";
+            });
+        }
+        else if (isOn === true) {
+            gridSquares[i].addEventListener("mouseover", function(e){
+                e.currentTarget.style.backgroundColor = toggleRandomColor();
+            });
+        };   
+    };
+});
 
 gridSizeButton.addEventListener('click', () => {
     let input = prompt("Enter the number of square per side (max 100): ")
